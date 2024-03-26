776147150f686988130b05c771dbae39e7193f22
Andreas <andreass@stacc.com>
2024-03-26 22:51:28 +0100

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    output: "export",
+    images: {
+        loader: 'akamai',
+        path: '',
+    },
+    assetPrefix: './',
 };
 
 export default nextConfig;
