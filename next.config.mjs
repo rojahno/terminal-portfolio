082e892f83578e665ab1c55cc0e076adb7e6799b
Andreas <andreass@stacc.com>
2024-03-26 22:57:48 +0100

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    images: {
-        loader: 'akamai',
-        path: '',
-    },
-    assetPrefix: './',
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
 };
 
 export default nextConfig;
