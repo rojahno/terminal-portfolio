347d73f171af1e0754d154f012a0cd4e5fe5e902
Andreas <andreass@stacc.com>
2024-03-26 23:04:52 +0100

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
+    output: "export",
+    basePath: "/nextjs-github-pages",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
